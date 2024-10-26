f332a51070fa798ddeafa42bd4119a100cec8d2b
jasonchandler3245643 <chandl12@go.stockton.edu>
2024-10-26 00:53:31 -0400

    Added the Monte Carlo Door Game
    The game is not final. I am just testing adding it

---
+++ Monte Carlo Door Game/DoorGameGUI.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+
+public class DoorGameGUI {
+
+	
+	private void createFrame() {
+		
+		JFrame options = new JFrame("ex");
+	
+		
+	}
+	
+	public void displayGame() {
+		
+		// create the frame
+		createFrame();
+		
+		
+		
+	}
+	
+	
+}
