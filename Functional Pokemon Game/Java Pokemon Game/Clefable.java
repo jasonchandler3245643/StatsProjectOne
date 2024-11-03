8678ab163c86fce20eedad17d6eb457505461f5a
jasonchandler3245643 <chandl12@go.stockton.edu>
2024-11-03 18:31:10 -0500

    Add files via upload

---
+++ Functional Pokemon Game/Java Pokemon Game/Clefable.java
@@ -0,0 +1,24 @@
+/**
+ * Represents a Clefable Pokemon Card
+ * 
+ * @author Jason Chandler
+ *
+ */
+public class Clefable extends Clefairy {
+	
+	/**
+	 * Constructs a Clefable with its stats
+	 * 
+	 */
+	public Clefable() {
+		
+		setHp(85);
+		setDamage(50);
+		setType(Type.Fairy);
+		setName("Clefable");
+		setStage(2);
+		
+	}
+	
+
+}
